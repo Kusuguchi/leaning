f0f9342926ee68581fb3a92e52902a94a09b985f
楠口恭佑 <kusuguchikyosuke@PS2-10191781-02.local>
2023-09-22 14:42:43 +0900

    add

---
+++ leaning.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+	id int PRIMARY KEY,
+	name varchar(10)
+);
+
+DROP TABLE users;
+
+INSERT INTO users(id,name) VALUES(10191781,'kusuguchi');
+INSERT INTO users(id,name) VALUES(10191782,'kusuguchi');
